5b79c43eca6fb007d19b296d8d05ddbe5bb95d2e
Aaron Den Hond <aaron.denhond@gmail.com>
2021-06-29 11:46:36 +0200

    new.ts updates

--- 0.S/classes/Car.js
+++ 0.S/classes/Car.js
@@ -12,6 +12,16 @@ var Car = /** @class */ (function () {
         enumerable: false,
         configurable: true
     });
+    Car.prototype.drive = function (engineStatus, fuel) {
+        if (engineStatus === false || fuel <= 0) {
+            //what I am doing here is a good principle called "failing early"
+            // If you have some conditions you need to check, that will exclude most of the code in your function check that first
+            // This prevents your "happy path" of code to be deeply indented.
+            return;
+        }
+        fuel -= 1;
+        this._miles += FUEL_MILEAGE;
+    };
     return Car;
 }());
 exports.Car = Car;
--- 0.S/classes/Car.ts
+++ 0.S/classes/Car.ts
@@ -1,13 +1,27 @@
+import { MusicPlayer } from "./MusicPlayer";
+import { Engine } from "./Engine";
+import { FuelTank } from "./FuelTank";
 export class Car {
 
     private _miles : number = 0;
 
- 
+
     get miles(): number {
         return this._miles;
     }
 
 
+    drive(engineStatus: boolean, fuel: number) {
+        if(engineStatus === false || fuel <= 0) {
+            //what I am doing here is a good principle called "failing early"
+            // If you have some conditions you need to check, that will exclude most of the code in your function check that first
+            // This prevents your "happy path" of code to be deeply indented.
+            return;
+        }
+        
+        fuel -= 1;
+        this._miles += FUEL_MILEAGE;
+    }
 
 
 }
